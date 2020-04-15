a14cc056306a55d3aeeea3fadf9d28f9ab74988d
Darshan Bakshi <darshanpamtap@gmail.com>
2020-04-15 20:39:43 +0530

    4th

---
+++ ex3.py
@@ -0,0 +1,19 @@
+print("chickens are:")
+print("hens:", 25 + 30 / 6) #"," is very very very crucial
+print("roosters:", 100-25+3%4) #"%" is for remaining in the division, in this case =3
+
+print("now counting eggs")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("is this true that 3+2<5-7:", 3+2<5-7)
+
+print("what's 3+2", 3+2)
+print("what's 5-7", 5-7)
+
+print("that's why false")
+
+
+print("is it greater?", 5>-2)
+print("is it greater or equal", 5>= -2)
+print("is it less or equal", 5<= -2)
