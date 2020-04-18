28ad0f27c48cb8995bbe94a86dd2d01d9cf1bbb0
Darshan Bakshi <darshanpamtap@gmail.com>
2020-04-18 22:15:59 +0530

    5th

---
+++ ex5.py
@@ -0,0 +1,22 @@
+my_name = "darshan.bakshi"
+my_age = 27
+my_height = 165 # cm
+my_weight = 80 #kg
+my_eyes = "brown"
+my_teeth = "white"
+my_hair = "black"
+
+print(f"lets talk about {my_name}")
+print(f"he is {my_height} tall")
+print(f"his weight is {my_weight}")
+
+print("that is heavy")
+
+print(f"eye color is {my_eyes}")
+print(f"teeth are {my_teeth}")
+print(f"with {my_hair} hair")
+
+#computing a different variable
+
+total = my_age + my_height + my_weight
+print(f"overall parameters are {my_age}, {my_height}, {my_weight}, {total}")
